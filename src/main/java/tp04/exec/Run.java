23e351f6aee31259a0caf5210ffb9eaf45f0291d
mehdibac <101841658+mehdibac@users.noreply.github.com>
2024-01-29 16:39:18 +0100

    Code Baccour Observ Senouci

--- src/main/java/tp04/exec/Run.java
+++ src/main/java/tp04/exec/Run.java
@@ -5,6 +5,7 @@
  */
 package tp04.exec;
 
+import java.util.ArrayList;
 import tp04.metier.Action;
 import tp04.metier.ActionComposee;
 import tp04.metier.ActionSimple;
@@ -12,20 +13,38 @@ import tp04.metier.Jour;
 import tp04.metier.Portefeuille;
 
 public class Run {
+    
+    public static void afficherBourse(ArrayList<Action> bourse, Jour j) {
+        System.out.println("Cours de la bourse pour le " + j.getNoJour() + "/" + j.getAnnee());
+        for (Action element : bourse) {
+            if (element.valeur(j) != 0.0f) {
+                System.out.println(element.getLibelle() + " : " + element.valeur(j));
+            }
+        }
+    }
 
     public static void main(String[] args) {
+        
+        ArrayList<Action> bourse = new ArrayList<>();
         ActionSimple bnp, axa;
         ActionComposee bqAss;
+        
         Jour j1, j2;
 
         // init des objets metiers Jour
         j1 = new Jour(2014, 1);
         j2 = new Jour(2014, 2);
-
+        
         // creation d'actions simples et composée
         bnp = new ActionSimple("BNP");
+        bourse.add(bnp);
         axa = new ActionSimple("AXA");
+        bourse.add(axa);
         bqAss = new ActionComposee("Banque-Assurance");
+        bourse.add(bqAss);
+        
+        
+        
         // enrg de la composition de l'action composée
         bqAss.enrgComposition(axa, 0.3f);
         bqAss.enrgComposition(bnp, 0.7f);
@@ -34,7 +53,7 @@ public class Run {
         axa.enrgCours(j2, 250);
         bnp.enrgCours(j1, 100);
         bnp.enrgCours(j2, 200);
-        // affichage des cours - comme 1 action simple et 1 action
+        // affichage des cours - comme 1 action simple et 1 action composee
         System.out.println("Action simple *bnp* à j1 : " + bnp.valeur(j1));
         System.out.println("Action *Banque-Assurance* à j2 : " + bqAss.valeur(j2));
 
@@ -57,6 +76,8 @@ public class Run {
         System.out.println("Portefeuille : " + p);
         p.vendre(bnp, 50);
         System.out.println("Portefeuille : " + p);
+        
+        afficherBourse(bourse, j1);
  
     }
 
---
+++ src/test/java/tp04/exec/RunTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2024 mehdi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tp04.exec;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import tp04.metier.Action;
+import tp04.metier.ActionComposee;
+import tp04.metier.ActionSimple;
+import tp04.metier.Jour;
+
+/**
+ *
+ * @author mehdi
+ */
+public class RunTest {
+    
+    public RunTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of afficherBourse method, of class Run.
+     */
+    @Test
+    public void testAfficherBourse() {
+        System.out.println("afficherBourse");
+        ArrayList<Action> bourse = new ArrayList<>();
+        ActionSimple bnp, axa, gog, rio;
+        ActionComposee bqAss, comp1, comp2;
+        
+        Jour j1, j2, j3, j4, j5;
+
+        // init des objets metiers Jour
+        j1 = new Jour(2014, 1);
+        j2 = new Jour(2014, 2);
+        j3 = new Jour(2014, 3);
+        j4 = new Jour(2015, 4);
+        j5 = new Jour(2018, 5);
+        
+        
+        // creation d'actions simples et composée
+        bnp = new ActionSimple("BNP");
+        bourse.add(bnp);
+        axa = new ActionSimple("AXA");
+        bourse.add(axa);
+        gog = new ActionSimple("Google");
+        bourse.add(gog);
+        rio = new ActionSimple("Riot");
+        bourse.add(rio);
+        
+        bqAss = new ActionComposee("Banque-Assurance");
+        bourse.add(bqAss);
+        comp1 = new ActionComposee("Compo1");
+        bourse.add(comp1);
+        comp2 = new ActionComposee("Compo2");
+        bourse.add(comp2);
+        
+        bourse.add(null);
+        
+        
+        
+        // enrg de la composition de l'action composée
+        bqAss.enrgComposition(axa, 0.3f);
+        bqAss.enrgComposition(bnp, 0.7f);
+        
+        comp1.enrgComposition(rio, 0.5f);
+        comp1.enrgComposition(axa, 0.6f);
+        
+        
+        comp2.enrgComposition(gog, 0.8f);
+        comp2.enrgComposition(rio, 0.0f);
+        // enrg. de 2 cours pour chaque action 
+        axa.enrgCours(j1, 200);
+        axa.enrgCours(j3, 400);
+        
+        bnp.enrgCours(j1, 100);
+        bnp.enrgCours(j3, 200);
+        
+        rio.enrgCours(j5, 505);
+        
+        gog.enrgCours(j3, 50);
+
+        Run.afficherBourse(bourse, j3);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Run.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        
+        
+        testAfficherBourse();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
